2bc998dd16f4ebd8e89442a08eac61303c2c2499
lf261 <luis.fernandez2093@gmail.com>
2020-07-16 15:52:53 -0400

    pushing code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -12,4 +13,16 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  fetchToys()
+
 });
+
+function fetchToys() {
+  fetch("http://localhost:3000/toys") 
+  .then(function(response){
+    return response.json(); 
+  })
+  .then(function(toys){
+    console.log(toys)
+  })
+}
