aafadf3a78a7e367ca12774650e596c4a0904c3a
Catrina Friday <cat.friday@gmail.com>
2020-07-16 17:31:41 -0400

    everything

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 let addToy = false;
 
+
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
@@ -24,5 +28,37 @@ function fetchToys() {
   })
   .then(function(toys){
     console.log(toys)
+    let toyCollect = document.getElementById('toy-collection')
+    console.log(toyCollect)
+
+    
+    //  buttonClass.value = 'like-btn'
+    // button.setAttributeNode(buttonClass)
+    
+    
+
+    toys.forEach(toy => {
+      let div = document.createElement('div')
+      div.className = 'card'
+      let h2 = document.createElement('h2')
+      let image = document.createElement('img')
+        image.className = 'toy-avatar'
+      let ptag = document.createElement('p')
+      let button = document.createElement('button')
+      button.className = 'like-btn'
+            h2.innerText = toy.name
+            div.append(h2)
+            image.src = toy.image
+            console.log(toy.image)
+            div.append(image)
+            ptag.innerText = toy.likes
+            div.append(ptag)
+            button.innerText = 'like'
+            div.append(button)
+
+            toyCollect.append(div)
+
+      });
+     
   })
 }
