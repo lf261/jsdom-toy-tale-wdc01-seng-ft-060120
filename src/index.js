51f4788216cb5f0834575504173b3d65aee8c09b
Catrina Friday <cat.friday@gmail.com>
2020-08-06 14:12:41 -0400

    did over

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 let addToy = false;
-
-
-
-
+url = "http://localhost:3000/toys/"
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -17,48 +14,103 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
-  fetchToys()
 
+  const toyForm = document.getElementsByClassName('add-toy-form')[0]
+  toyForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    formSubmit(toyForm)
+    toyForm.reset()
+  })
+  
+  fetchToys()
 });
 
+
+
 function fetchToys() {
-  fetch("http://localhost:3000/toys") 
+  fetch(url) 
   .then(function(response){
     return response.json(); 
   })
   .then(function(toys){
-    console.log(toys)
-    let toyCollect = document.getElementById('toy-collection')
-    console.log(toyCollect)
-
-    
-    //  buttonClass.value = 'like-btn'
-    // button.setAttributeNode(buttonClass)
-    
-    
-
-    toys.forEach(toy => {
-      let div = document.createElement('div')
-      div.className = 'card'
-      let h2 = document.createElement('h2')
-      let image = document.createElement('img')
-        image.className = 'toy-avatar'
-      let ptag = document.createElement('p')
-      let button = document.createElement('button')
-      button.className = 'like-btn'
-            h2.innerText = toy.name
-            div.append(h2)
-            image.src = toy.image
-            console.log(toy.image)
-            div.append(image)
-            ptag.innerText = toy.likes
-            div.append(ptag)
-            button.innerText = 'like'
-            div.append(button)
-
-            toyCollect.append(div)
-
-      });
-     
+    renderToys(toys)
   })
 }
+
+function renderToys(toys){
+  toys.forEach(toy => {
+    renderToy(toy)
+  })
+}
+
+function renderToy(toy){
+   let toyCollect = document.getElementById('toy-collection')
+
+      let div = document.createElement('div')
+      div.className = 'card'
+
+      let h2 = document.createElement('h2')
+      h2.innerText = toy.name
+      div.append(h2)
+
+      let image = document.createElement('img')
+      image.className = 'toy-avatar'
+      image.src = toy.image
+      div.append(image)
+
+      let ptag = document.createElement('p')
+      ptag.innerText = `${toy.likes} Likes`
+      div.append(ptag)
+
+      let likeButton = document.createElement('button')
+      likeButton.className = 'like-btn'
+      likeButton.innerText = 'like'
+      div.append(likeButton)
+      likeButton.addEventListener("click", (e) => {
+         increaseLikes(toy, ptag)
+      })
+            
+      toyCollect.append(div)
+}
+
+function increaseLikes(toy, ptag) {
+   console.log(toy)
+  fetch(url +`${toy.id}`, {
+    method: 'put',
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: toy.name,
+      image: toy.image,
+      likes: toy.likes += 1
+    })
+  })
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(toy) {
+    ptag.innerText = `${toy.likes} Likes`}
+)}
+
+function formSubmit(toyForm){
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: toyForm.name.value,
+      image: toyForm.image.value,
+      likes: 0  
+    })
+  })
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(toy) {
+   renderToy(toy)}
+  )}
+
